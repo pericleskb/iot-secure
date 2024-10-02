38b520db20d5da9becf95e9cb89aae695628326d
pericles <pericleskb@gmail.com>
2024-10-02 22:30:43 +0300

    add mqtt subscriber

--- init.sh
+++ init.sh
@@ -8,5 +8,7 @@ set -e
 python manage.py makemigrations web_server
 python manage.py migrate
 
+exec python iot_server/mqtt/measurementsSubscriber.py &
+
 # Start the Django application
 exec python manage.py runserver 0.0.0.0:8000

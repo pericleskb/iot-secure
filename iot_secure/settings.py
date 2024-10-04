66d6ad17d206ef898cf4bd45e0a5df495551a88f
pericles <pericleskb@gmail.com>
2024-10-04 14:13:25 +0300

    add allowed hosts

--- iot_secure/settings.py
+++ iot_secure/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lgm@aa)&z*zb&%(aw(rro3p)j6id$^_puhfor2hho6ox1z0j1c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.19', 'raspberrypi.local']
 
 
 # Application definition

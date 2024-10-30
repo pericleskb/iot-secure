cb5be76cfff301ad1a11375e959681c7d50c4f3a
pericles <pericleskb@gmail.com>
2024-10-30 12:14:14 +0200

    fix indendation of createDefaultFiles.py

--- web_server_project/iot_server/files/createDefaultFiles.py
+++ web_server_project/iot_server/files/createDefaultFiles.py
@@ -1,31 +1,30 @@
 import os
 
-def create_certificates_conf_file():
-    # Get the user's home directory
-    home_dir = os.path.expanduser("~")
+# Get the user's home directory
+home_dir = os.path.expanduser("~")
 
-    # Full path to the directory
-    dir_path = os.path.join(home_dir, "ssl")
+# Full path to the directoryd
+dir_path = os.path.join(home_dir, "ssl")
 
-    # Create the directory if it doesn't exist
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-        print(f"Directory created at {dir_path}")
+# Create the directory if it doesn't exist
+if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
+    print(f"Directory created at {dir_path}")
 
-    # Full path to the file
-    file_path = os.path.join(dir_path, "certificates.conf")
+# Full path to the file
+file_path = os.path.join(dir_path, "certificates.conf")
 
-    # Check if the file exists
-    if not os.path.exists(file_path):
-        # Content to add to the file
-        file_content = """#ca_certs=path_to_authority_certificate
-        #certfile=path_to_device_certificate
-        #keyfile=path_to_device_key"""
+# Check if the file exists
+if not os.path.exists(file_path):
+    # Content to add to the file
+    file_content = """#ca_certs=path_to_authority_certificate
+    #certfile=path_to_device_certificate
+    #keyfile=path_to_device_key"""
 
-        # Create the file and write the content
-        with open(file_path, 'w') as file:
-            file.write(file_content)
+    # Create the file and write the content
+    with open(file_path, 'w') as file:
+        file.write(file_content)
 
-        print(f"File created at {file_path}")
-    else:
-        print(f"File already exists at {file_path}")
+    print(f"File created at {file_path}")
+else:
+    print(f"File already exists at {file_path}")

65a1188d16a8b706f9c7cc971a9757ee0709bacd
pericles <pericleskb@gmail.com>
2024-10-31 17:18:26 +0200

    add selected option table

--- web_server_project/web_server/models.py
+++ web_server_project/web_server/models.py
@@ -5,13 +5,24 @@ class User(models.Model):
         password = models.CharField(max_length=256)
         
         def __str__(self):
-                return self.username + ":" + self.password
+                return f"self.username:self.password"
 
 
 class SecurityOptions(models.Model):
         option_code = models.CharField(max_length=100)
         option_description = models.CharField(max_length=100)
         option_text = models.CharField(max_length=2000)
+        isSelected = models.BooleanField(False)
 
         def __str__(self):
-                return self.option_code + ":" + self.option_description
+                return f"self.option_code:self.option_description"
+
+class SelectedOption(models.Model):
+    option = models.OneToOneField(
+        SecurityOptions,
+        on_delete=models.CASCADE,
+        primary_key=True,  # Ensures only one row in this table
+    )
+
+    def __str__(self):
+        return f"Selected Option: {self.option.name}"

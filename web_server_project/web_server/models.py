b00032712cbdd0ec279e3457243f0a0b72f86658
pericles <pericleskb@gmail.com>
2024-10-31 17:55:02 +0200

    populate db on startup

--- web_server_project/web_server/models.py
+++ web_server_project/web_server/models.py
@@ -12,7 +12,6 @@ class SecurityOptions(models.Model):
         option_code = models.CharField(max_length=100)
         option_description = models.CharField(max_length=100)
         option_text = models.CharField(max_length=2000)
-        isSelected = models.BooleanField(False)
 
         def __str__(self):
                 return f"self.option_code:self.option_description"

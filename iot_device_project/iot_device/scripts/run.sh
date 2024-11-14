63b509051fada569628f7b4fff14d9fc0bfccefa
periklis <pericleskb@gmail.com>
2024-11-14 12:16:37 +0200

    add new after requesting password

--- iot_device_project/iot_device/scripts/run.sh
+++ iot_device_project/iot_device/scripts/run.sh
@@ -4,5 +4,5 @@ source ../iot-device-venv/bin/activate
 
 read -p "Enter device name: " name
 read -s -p "Enter private key encryption password: " password
-
+echo ""
 python ../main.py $name $password

c37c9d269cdffae05649e3c8cf5a66c079295270
pericles <pericleskb@gmail.com>
2024-11-16 19:11:17 +0200

    add inserting device name once

--- iot_device_project/iot_device/scripts/run.sh
+++ iot_device_project/iot_device/scripts/run.sh
@@ -2,7 +2,31 @@
 
 source ../iot-device-venv/bin/activate
 
-read -p "Enter device name: " name
-read -s -p "Enter private key encryption password: " password
+#!/bin/bash
+
+# Define the file path
+FILE="name.txt"
+device_name=""
+
+# Check if the file exists
+if [[ -f "$FILE" ]]; then
+    # File exists, read the first line
+    first_line=$(head -n 1 "$FILE")
+
+    if [[ -n "$first_line" ]]; then
+        # First line is not empty, save it to a variable
+        device_name="$first_line"
+    else
+        # First line is empty, prompt user for a name
+        read -p "Enter device name: " name
+        echo "$name" > "$FILE"
+        device_name="$name"
+    fi
+else
+    # File does not exist, prompt user for a name
+    read -p "Enter device name: " name
+    read user_name
+    echo "$user_name" > "$FILE"
+fi
 echo ""
-python ../main.py $name $password
+python ../main.py "$device_name" "$password"

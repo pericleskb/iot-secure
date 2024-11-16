7b2d3a691cce551fa50609540056348dd9d6b5aa
periklis <pericleskb@gmail.com>
2024-11-16 19:21:28 +0200

    place device name under home dir

--- iot_device_project/iot_device/scripts/run.sh
+++ iot_device_project/iot_device/scripts/run.sh
@@ -5,14 +5,14 @@ source ../iot-device-venv/bin/activate
 #!/bin/bash
 
 # Define the file path
-FILE="name.txt"
+FILE="$HOME/iot_secure/name.txt"
+mkdir -p "$HOME/iot_secure"
 device_name=""
 
 # Check if the file exists
 if [[ -f "$FILE" ]]; then
     # File exists, read the first line
     first_line=$(head -n 1 "$FILE")
-
     if [[ -n "$first_line" ]]; then
         # First line is not empty, save it to a variable
         device_name="$first_line"
@@ -25,8 +25,9 @@ if [[ -f "$FILE" ]]; then
 else
     # File does not exist, prompt user for a name
     read -p "Enter device name: " name
-    read user_name
-    echo "$user_name" > "$FILE"
+    echo "$name" > "$FILE"
+    device_name="$name"
 fi
+read -s -p "Enter private key encryption password: " password
 echo ""
 python ../main.py "$device_name" "$password"

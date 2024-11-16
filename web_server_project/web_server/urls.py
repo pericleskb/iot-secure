0ce6518e39a7489ee5828b24fc0b532214ac767f
pericles <pericleskb@gmail.com>
2024-11-16 17:55:28 +0200

    Revert "add log in log out function"
    This reverts commit df86f7a5c0e9fda35a8b96f5f82c1bdea62c8ae0.

--- web_server_project/web_server/urls.py
+++ web_server_project/web_server/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,8 +6,7 @@ app_name = "web_server"
 urlpatterns = [
     path('', views.home, name="home"),
     path('register/', views.register, name="register"),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login_view"),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout_view'),
+    path('login/', views.login, name="login"),
     path('login-ajax/', views.login_ajax, name="login_ajax"),
     path('settings/', views.settings, name="settings"),
     path('api/options/', views.get_options, name='get_options'),
